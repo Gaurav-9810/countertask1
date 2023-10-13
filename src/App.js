0db5c59fe71eb19bd1e4338400a7c08c32c27551
guarav <gauravgangola444@gmail.com>
2023-10-13 21:25:21 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setcount] = useState(0)
+
+
+  return (
+    <div className="App">
+      <h1></h1>
+      
+    </div>
+  );
+}
+
+export default App;
